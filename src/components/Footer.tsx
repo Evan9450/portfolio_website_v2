828e1b3e7ad0ffd5322f1afea383ca671839fafd
Evan9450 <evan9450@gmail.com>
2025-09-11 16:20:14 +1000

    Added: sharp for image optimize

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-end items-center">
           <div className="mb-4 md:mb-0">
                       <p className="text-sm text-muted-foreground">
-            © {new Date().getFullYear()}  {siteConfig.name}. All rights reserved.
+            © {new Date().getFullYear()} {siteConfig.name}. All rights reserved.
           </p>
           </div>
           {/* <div className="flex gap-4">
